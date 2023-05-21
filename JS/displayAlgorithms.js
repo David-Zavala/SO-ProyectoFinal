52a8b4adeee93ca028bb1af1482fc785cfe542b6
David-Zavala <david.zavalal@udem.edu>
2023-05-20 22:50:58 -0600

    Correccion de errores al desplegar algoritmos

--- JS/displayAlgorithms.js
+++ JS/displayAlgorithms.js
@@ -8,11 +8,11 @@ const HRRNElement = document.getElementById('HRRN');
 const MLFQElement = document.getElementById('MLFQ');
 fetch('Algs-Sched/FCFS.html').then(response => response.text()).then(html => {FCFSElement.innerHTML = html;});
 fetch('Algs-Sched/FIFO.html').then(response => response.text()).then(html => {FIFOElement.innerHTML = html;});
-fetch('Algs-Sched/RR.html').then(response => response.text()).then(html => {FIFOElement.innerHTML = html;});
-fetch('Algs-Sched/SJF.html').then(response => response.text()).then(html => {FIFOElement.innerHTML = html;});
-fetch('Algs-Sched/SRT.html').then(response => response.text()).then(html => {FIFOElement.innerHTML = html;});
-fetch('Algs-Sched/HRRN.html').then(response => response.text()).then(html => {FIFOElement.innerHTML = html;});
-fetch('Algs-Sched/MLFQ.html').then(response => response.text()).then(html => {FIFOElement.innerHTML = html;});
+fetch('Algs-Sched/RR.html').then(response => response.text()).then(html => {RRElement.innerHTML = html;});
+fetch('Algs-Sched/SJF.html').then(response => response.text()).then(html => {SJFElement.innerHTML = html;});
+fetch('Algs-Sched/SRT.html').then(response => response.text()).then(html => {SRTElement.innerHTML = html;});
+fetch('Algs-Sched/HRRN.html').then(response => response.text()).then(html => {HRRNElement.innerHTML = html;});
+fetch('Algs-Sched/MLFQ.html').then(response => response.text()).then(html => {MLFQElement.innerHTML = html;});
 //Referencias a los archivos de los Algoritmos Paginacion
 const FIFOapElement = document.getElementById('FIFOap');
 const LRUapElement = document.getElementById('LRUap');
