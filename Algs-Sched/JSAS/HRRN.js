6416f48edc18c84d66823796ba3fff887bca6326
Hans Nudding <hansndt98@gmail.com>
2023-05-22 20:14:50 -0600

    Finishing touches

--- Algs-Sched/JSAS/HRRN.js
+++ Algs-Sched/JSAS/HRRN.js
@@ -1,129 +1,146 @@
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 class Process {
-  constructor(){
-      this.pid = '#';
-      this.at = 0;
-      this.bt = 0;
-      this.ct = 0;
-      this.wt = 0;
-      this.tt = 0;
-      this.completed= 0;
-      this.ntt = 0;
+  constructor(pid, arrivalTime, burstTime, completed) {
+      this.pid = pid;
+      this.arrivalTime = arrivalTime;
+      this.burstTime = burstTime;
+      this.remainingTime = burstTime;
+      this.waitTime = 0;
+      this.turnaroundTime = 0;
+      this.completionTime = 0;
+      this.completed = 0;
   }
-
-}
-let p = new Array(10);
-for(let i = 0; i < 10; i++){
-  p[i] = new Process();
 }
 
-let n;
-
-// Sorting Processes by Arrival Time
-function sortByArrival()
+function sortByArrival(a, b)
 {
-  let temp;
-  let i, j;
+  return a.arrivalTime - b.arrivalTime;
+}
 
-  // Selection Sort applied
-  for (i = 0; i < n - 1; i++) {
-      for (j = i + 1; j < n; j++) {
+async function simulateHRRN(processes){
 
-          // Check for lesser arrival time
-          if (p[i].at > p[j].at) {
+  // Si el reloj no esta iniciado, iniciarlo
+  let clockInput = document.querySelector('.clock');
+  let clockValue = clockInput.value;
+  if (clockValue == 0) startClock();
+  
+  // Verificar que el reloj no este detenido
+  stopClock();
+  startClock();
 
-              // Swap earlier Process to front
-              temp = p[i];
-              p[i] = p[j];
-              p[j] = temp;
+  // Suma de los bursts
+  let sumBurst = 0; let n = processes.length;
+  for(i=0; i<n; i++){
+    sumBurst += processes[i].burstTime;
+  }
+  
+  // Ciclo del proceso HRRN
+  for (currentTime = 0; currentTime < sumBurst;) {
+    // Ajustar Response-Rate
+    let hrr = -9999;
+    let respRatio;
+
+
+    let nextP;
+    for (i = 0; i < n; i++) {
+        if (processes[i].arrivalTime <= currentTime && processes[i].completed != 1) {
+            displayResult(processes, currentTime, processes[i].pid);
+
+            // Calculamos Response-Ratio
+            respRatio = (processes[i].burstTime + (currentTime - processes[i].arrivalTime)) / processes[i].burstTime;
+
+            // Checamos el highest response-ratio
+            if (hrr < respRatio) {
+                // Storing Response Ratio
+                hrr = respRatio;
+                nextP = i; // Siguiente proceso
+            }
+        }
+    }
+    
+    
+    // Actualizar timer y esperar el tiempo de burst del proceso
+    for(j=0; j<processes[nextP].burstTime; j++){
+        // Manejador de Interrupciones
+      const message = document.querySelector(".messagesHRRN");
+      let inter = document.querySelector('.interrupt').value;
+      if (inter > 0){
+          switch (inter){
+              case '1':
+                message.innerHTML += `<input disabled readonly class="form-control" value="Se ha interrumpido el proceso: ${processes[nextP].pid}, a través de SVC I/O">`;
+                break;
+              case '2':
+                message.innerHTML += `<input disabled readonly class="form-control" value="Se ha interrumpido el proceso: ${processes[nextP].pid}, a traves de SVC Normal">`;
+                break;
+              case '3':
+                message.innerHTML += `<input disabled readonly class="form-control" value="Se ha interrumpido el proceso: ${processes[nextP].pid}, a traves de un error del programa">`;
+                break;
+              case '4':
+                message.innerHTML += `<input disabled readonly class="form-control" value="El proceso: ${processes[nextP].pid}, se ha convertido en un proceso Zoombie">`;
+                break;
+              case '5':
+                message.innerHTML += `<input disabled readonly class="form-control" value="Se ha interrumpido el proceso: ${processes[nextP].pid} abruptamente">`;
+                break;
           }
+          document.querySelector('.interrupt').value = 0;
+          currentTime++;
+          await sleep(1000);
+      } else {
+        await sleep(1000);
+        processes[nextP].remainingTime--;
+        currentTime++;
+        displayResult(processes, currentTime, processes[nextP].pid);
       }
+    }
+
+    // Actualizar variables
+    processes[nextP].waitTime = currentTime - processes[nextP].arrivalTime - processes[nextP].burstTime;
+    processes[nextP].turnaroundTime = currentTime - processes[nextP].arrivalTime;
+    processes[nextP].completed = 1;
+    processes[nextP].completionTime = currentTime;
+
+    displayResult(processes, currentTime, processes[nextP].pid);
+  }
+
+  stopClock();
+}
+
+async function ejecutarHRRN(){
+  const n = document.getElementsByClassName("PIDhrrn").length;
+  let processes = [];
+
+  for (let i = 0; i < n; i++) {
+      let burstTime = parseInt(document.getElementsByClassName("B_Thrrn")[i].value);
+      let arrivalTime = parseInt(document.getElementsByClassName("A_Thrrn")[i].value);
+
+      if (isNaN(burstTime) || isNaN(arrivalTime)) {
+          continue;
+      }
+      processes.push(new Process(i + 1, arrivalTime, burstTime, 0));
+    }
+    processes.sort(sortByArrival);
+
+    await simulateHRRN(processes);
+}
+
+async function displayResult(processes) {
+  const pidrs = document.querySelectorAll ('.hrrnPIDr');
+  const atrs = document.querySelectorAll('.hrrnA_Tr');
+  const btrs = document.querySelectorAll('.hrrnB_Tr');
+  const rtrs = document.querySelectorAll('.hrrnR_Tr');
+  const wtrs = document.querySelectorAll('.hrrnW_Tr');
+  const tatrs = document.querySelectorAll('.hrrnTA_Tr');
+  const currPid = document.getElementsByClassName('hrrnPIDcr');
+
+  for(let i = 0; i < processes.length; i++) {
+      pidrs[i].value = processes[i].pid;
+      atrs[i].value = processes[i].arrivalTime;
+      btrs[i].value = processes[i].burstTime;
+      rtrs[i].value = processes[i].remainingTime;
+      wtrs[i].value = processes[i].waitTime;
+      tatrs[i].value = processes[i].turnaroundTime;
   }
 }
-
-
-let i, j, sum_bt = 0;
-let c;
-let t, avgwt = 0, avgtt = 0;
-n = 5;
-
-// predefined arrival times
-let arriv = [0, 2, 4, 6, 8];
-
-// predefined burst times
-let burst = [3, 6, 4, 5, 2];
-
-// Initializing the structure variables
-for (i = 0, c = 'A'; i < n; i++) {
-  p[i].name = c;
-  p[i].at = arriv[i];
-  p[i].bt = burst[i];
-
-  // Variable for Completion status
-  // Pending = 0
-  // Completed = 1
-  p[i].completed = 0;
-
-  // Variable for sum of all Burst Times
-  sum_bt += p[i].bt;
-  c = String.fromCharCode(c.charCodeAt(0) + 1);
-}
-
-// Sorting the structure by arrival times
-sortByArrival();
-console.log("PN\tAT\tBT\tWT\tTAT\tNTT");
-
-for (t = p[0].at; t < sum_bt;) {
-
-  // Set lower limit to response ratio
-  let hrr = -9999;
-
-  // Response Ratio Variable
-  let temp;
-
-  // Variable to store next Process selected
-  let loc;
-  for (i = 0; i < n; i++) {
-
-      // Checking if Process has arrived and is
-      // Incomplete
-      if (p[i].at <= t && p[i].completed != 1) {
-
-          // Calculating Response Ratio
-          temp = (p[i].bt + (t - p[i].at)) / p[i].bt;
-
-          // Checking for Highest Response Ratio
-          if (hrr < temp) {
-
-              // Storing Response Ratio
-              hrr = temp;
-
-              // Storing Location
-              loc = i;
-          }
-      }
-  }
-
-  // Updating time value
-  t += p[loc].bt;
-
-  // Calculation of waiting time
-  p[loc].wt = t - p[loc].at - p[loc].bt;
-
-  // Calculation of Turn Around Time
-  p[loc].tt = t - p[loc].at;
-
-  // Sum Turn Around Time for average
-  avgtt += p[loc].tt;
-
-  // Calculation of Normalized Turn Around Time
-  p[loc].ntt = (p[loc].tt / p[loc].bt);
-
-  // Updating Completion Status
-  p[loc].completed = 1;
-
-  // Sum Waiting Time for average
-  avgwt += p[loc].wt;
-  console.log(p[loc].name + "\t" + p[loc].at + "\t" + p[loc].bt + "\t" + p[loc].wt + "\t" + p[loc].tt + "\t" + p[loc].ntt);
-}
-console.log("\nAverage waiting time: " + avgwt / n);
-console.log("Average Turn Around time:" + avgtt / n);
